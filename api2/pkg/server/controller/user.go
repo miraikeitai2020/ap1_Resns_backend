241c9305164394b53876de18354c45e6d30c19a6
daisukesakai100000 <daisuke.sakai.2p@stu.hosei.ac.jp>
2020-10-07 23:40:33 +0900

    wip

--- api2/pkg/server/controller/user.go
+++ api2/pkg/server/controller/user.go
@@ -1,8 +1,6 @@
 package controller
 
 import (
-	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -10,16 +8,9 @@ import (
 
 func HandleUserSet() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		err := c.ShouldBindJSON(&articleRequest)
-		if err != nil {
-			log.Println("[ERROR] Faild Bind JSON")
-			c.JSON(500, gin.H{"message": "Internal Server Error"})
-			return
-		}
-		articles, err := model.GetArticles(articleRequest.Genre, articleRequest.Month, articleRequest.Year)
-		fmt.Println(articles)
+
 		// // 生成した認証トークンを返却
-		c.JSON(http.StatusOK, "")
+
 	}
 }
 
