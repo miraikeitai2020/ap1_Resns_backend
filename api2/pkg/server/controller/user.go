2fd07a415bcb89c08279b6431ba88afcbffa7b47
daisukesakai100000 <daisuke.sakai.2p@stu.hosei.ac.jp>
2020-10-08 18:49:23 +0900

    [wip]user.go

--- api2/pkg/server/controller/user.go
+++ api2/pkg/server/controller/user.go
@@ -6,17 +6,39 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+//ユーザー情報
 func HandleUserSet() gin.HandlerFunc {
 	return func(c *gin.Context) {
-
+		if err := context.BindJSON(&HandleUserSet); err != nil {
+			context.JSON(500, gin.H{"message": "Internal Server Error"})
+			return
+		}
+		
+		uuid, err := uuid.NewRandom()
+		if err != nil {
+			context.JSON(http.StatusInternalServerError, "AuthToken is error")
+			return
+		}
+		restoken.Token = uuid.String()
+		if err := HandleUserUpdate(HandleUserSet.Id, HandleUserSet.Name, HandleUserSet.Image, HandleUserSet.Year, HandleUserSet.Month HandleUserSet.Gender , restoken.Token); err != nil {
+			context.JSON(http.StatusInternalServerError, "Internal Server Error")
+			return
+		}
 		// // 生成した認証トークンを返却
+		c.JSON(http.StatusOK,"" )
+	}	
 
-	}
 }
-
-func HandleUserUpdate() gin.HandlerFunc {
+//ユーザー情報更新
+func HandleUserUpdate(id, name, image, token string, year, month, gender int) gin.HandlerFunc {
 	return func(c *gin.Context) {
+		stmt, err := db.Con.Prepare("INSERT INTO UserSet VALUES (?,?,?)")
+		if err != nil {
+			return err
+		}
+		_, err = stmt.Exec(id, name, image, token, year, month, gender)
+		return err
 		// // 生成した認証トークンを返却
-		c.JSON(http.StatusOK)
+		c.JSON(http.StatusOK,"" )
 	}
 }
--- api2/pkg/server/model/user.go
+++ api2/pkg/server/model/user.go
@@ -1,48 +1,15 @@
 package model
 
-import (
-	"ResnsBackend-api2/pkg/db"
-	"database/sql"
-	"log"
-)
-
 //  userテーブルデータ
 type User struct {
-	Id string `json:"id" `
-	Name string `json:"name" `
-	Image string `json:"image" `
-	Year int `json:"year" `
-	month int `json:"month" `
-	Gender int `json:"gender" `
+	Id     string `json:"id" `
+	Name   string `json:"name" `
+	Image  string `json:"image" `
+	Year   int    `json:"year" `
+	month  int    `json:"month" `
+	Gender int    `json:"gender" `
 }
 
 type Retoken struct {
 	Token string `json:"token" `
 }
-
-func (ctrl *Controller) SetUser(context *gin.Context) {
-	if err := context.BindJSON(&SetUser); err != nil {
-		context.JSON(500, gin.H{"message": "Internal Server Error"})
-		return
-	}
-	
-	uuid, err := uuid.NewRandom()
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, "AuthToken is error")
-		return
-	}
-	restoken.Token = uuid.String()
-	if err := UpdateUser(SetUser.Id, SetUser.Name, SetUser.Image, SetUser.Year, SetUser.Month SetUser.Gender , restoken.Token); err != nil {
-		context.JSON(http.StatusInternalServerError, "Internal Server Error")
-		return
-	}
-	context.JSON(200, restoken)
-}
-
-func UpdateUser(id, name, image, token string, year, month, gender int) error {
-	stmt, err := db.Con.Prepare("INSERT INTO signUp VALUES (?,?,?)")
-	if err != nil {
-		return err
-	}
-	_, err = stmt.Exec(id, name, token, year, month, gender)
-	return err
